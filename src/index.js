ee80d907b01390516931e83d4703048ecfaefe48
Pavel Korchyk <Pavel.Korchyk@itechart-group.com>
2018-07-24 16:05:16 +0300

    just build version

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CounterApp from './containers/CounterContainer.js';
-
-ReactDOM.render(
-    <CounterApp />,
-    document.getElementById('root')
-);
